4975f3f26c2f3c644ce5afdf64afe53b51baeff4
Kovács Dániel <danifreecs@gmail.com>
2018-06-21 12:48:06 +0200

    Yogi Bear game

---
+++ YogiBearWPF/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using YogiBearWPF.Model;
+using YogiBearWPF.Persistence;
+using YogiBearWPF.View;
+using YogiBearWPF.ViewModel;
+
+namespace YogiBearWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Fields
+
+        private YogiBearModel model;
+        private YogiBearViewModel viewModel;
+        private MainWindow view;
+
+        //Ctor
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            //modell létrehozás
+            model = new YogiBearModel();
+
+            //viewmodell létrehozás
+            viewModel = new YogiBearViewModel(model);
+            viewModel.NewGame += new EventHandler(ViewModel_NewGame);
+            viewModel.ExitGame += new EventHandler(ViewModel_ExitGame);
+            viewModel.VM_GameOver += new EventHandler<GameOverEventArgs>(ViewModel_GameOver);
+
+            //nézet létrehozása
+            view = new MainWindow();
+            view.KeyDown += new KeyEventHandler(ViewModel_OnKeyDownHandler);
+            view.DataContext = viewModel;
+            view.Show();
+        }
+
+        // gomb lenyomásának kezelője
+        private void ViewModel_OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            switch(e.Key)
+            {
+                case Key.W: //fel
+                    viewModel.MoveBear(1);
+                    break;
+                case Key.A: //balra
+                    viewModel.MoveBear(4);
+                    break;
+                case Key.S: //le
+                    viewModel.MoveBear(2);
+                    break;
+                case Key.D: //jobbra
+                    viewModel.MoveBear(3);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ViewModel_NewGame(object sender, EventArgs e)
+        {
+            model.NewGame();
+        }
+
+        private void ViewModel_ExitGame(object sender, System.EventArgs e)
+        {
+            view.Close(); // ablak bezárása
+        }
+
+        private void ViewModel_GameOver(object sender, GameOverEventArgs e)
+        {
+            if (e.result)
+                MessageBox.Show("Gratulálok, győztél!", "Győzelem", MessageBoxButton.OK);
+            else
+                MessageBox.Show("Sajnos vesztettél :(", "Kudarc", MessageBoxButton.OK);
+        }
+    }
+}
---
+++ YogiBearWPF/ViewModel/YogiBearViewModel.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using YogiBearWPF.Model;
+
+namespace YogiBearWPF.ViewModel
+{
+    //NézetModell típusa
+    public class YogiBearViewModel : ViewModelBase
+    {
+        private YogiBearModel model;
+        private bool paused = true;
+
+        //Properties
+        public DelegateCommand NewGameCommand { get; private set; }
+        public DelegateCommand PauseCommand { get; private set; }
+        public DelegateCommand ExitCommand { get; private set; }
+        public ObservableCollection<GameField> Fields { get; set; }
+
+        //játékidő
+        public String GameTime { get { return TimeSpan.FromSeconds(model.GameTime).ToString(@"mm\:ss"); } }
+        //Kosarak száma
+        public String Baskets { get { return (model.Baskets).ToString(); } }
+
+        //Events
+        public event EventHandler NewGame;
+        public event EventHandler ExitGame;
+        public event EventHandler<GameOverEventArgs> VM_GameOver;
+        
+        //Constructor
+        public YogiBearViewModel(YogiBearModel m)
+        {
+            model = m;
+            model.GameOver += new EventHandler<GameOverEventArgs>(OnVM_GameOver);
+            NewGameCommand = new DelegateCommand(param => { OnNewGame(); SetupTable(); });
+            PauseCommand = new DelegateCommand(param => { OnPause(); });
+            ExitCommand = new DelegateCommand(param => OnExitGame());
+            Fields = new ObservableCollection<GameField>();
+            model.Refresh += RefreshTable;
+        }
+
+        //Játékmezők frissítése
+        private void RefreshGameFields()
+        {
+            foreach (GameField f in Fields)
+                f.Value = model.Map[f.X][f.Y];
+        }
+
+        //Játéktábla frissítésének eseménykezelője
+        private void RefreshTable(object sender, EventArgs e)
+        {
+            App.Current.Dispatcher.BeginInvoke((System.Action)delegate
+            {
+                RefreshGameFields();
+
+                OnPropertyChanged("Fields");
+                OnPropertyChanged("GameTime");
+                OnPropertyChanged("Baskets");
+            });
+        }
+
+        //Játéktáblát reprezentáló collection feltöltése
+        private void SetupTable()
+        {
+            Fields.Clear();
+            for (Int32 i = 0; i < model.Map.Count; i++)
+            {
+                for (Int32 j = 0; j < model.Map.Count; j++)
+                {
+                    Fields.Add(new GameField
+                    {
+                        Value = model.Map[i][j],
+                        X = i,
+                        Y = j
+                    });
+                }
+            }
+            OnPropertyChanged("Fields");
+            OnPropertyChanged("GameTime");
+            OnPropertyChanged("Baskets");
+        }
+
+
+        //Event methods
+        private void OnPause()
+        {
+            paused = !paused;
+
+            if(paused)
+            {
+                model.time.Stop();
+                model.patrolling.Stop();
+            }else
+            {
+                model.time.Start();
+                model.patrolling.Start();
+            }
+        }
+
+
+        // Új játék indításának eseménykiváltása.
+        private void OnNewGame()
+        {
+            if(paused)
+            {
+                paused = !paused;
+            }
+
+            if (NewGame != null)
+                NewGame(this, EventArgs.Empty);
+        }
+
+        //Kilépés
+        private void OnExitGame()
+        {
+            if (ExitGame != null)
+                ExitGame(this, EventArgs.Empty);
+        }
+
+        //Játékos mozgása
+        public void MoveBear(int direction)
+        {
+            if(!paused)
+            { 
+                switch (direction)
+                {
+                    case 1:
+                        if (model.IsFloor(model.PlayerPos.X - 1, model.PlayerPos.Y)) model.Up();
+                        break;
+                    case 2:
+                        if (model.IsFloor(model.PlayerPos.X + 1, model.PlayerPos.Y)) model.Down();
+                        break;
+                    case 3:
+                        if (model.IsFloor(model.PlayerPos.X, model.PlayerPos.Y + 1)) model.Right();
+                        break;
+                    case 4:
+                        if (model.IsFloor(model.PlayerPos.X, model.PlayerPos.Y - 1)) model.Left();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            RefreshGameFields();
+            OnPropertyChanged("Fields");
+        }
+
+        //Játék vége eseménykezelője
+        private void OnVM_GameOver(object sender, GameOverEventArgs e)
+        {
+            OnPause();
+
+            if(VM_GameOver != null)
+            {
+                VM_GameOver(this, new GameOverEventArgs(e.result));
+            }
+        }
+
+    }
+
+}
